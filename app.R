07935286203fb0a9448a3c90ac333a59ccc4d6f0
Michael Cawthon <michael_cawthon@Michaels-MBP.attlocal.net>
2020-12-03 10:46:11 -0600

    Changed title

--- app.R
+++ app.R
@@ -13,7 +13,7 @@ library(shiny)
 ui <- fluidPage(
 
     # Application title
-    titlePanel("INDRA"),
+    titlePanel("INDRA Site Alpha"),
 
     # Sidebar with a slider input for number of bins 
     sidebarLayout(
