1990476de14a8c9f9ba9772def1b852afac0e41a
Michael Cawthon <michael_cawthon@Michaels-MBP.attlocal.net>
2020-12-12 12:27:46 -0600

    debugging on VM (works locally)

--- app.R
+++ app.R
@@ -44,35 +44,35 @@ ui <- fluidPage(
  
 
 
-### we'll also need historical data for SPY
-
-spy_history <- indratools2::td_market_value_traded("SPY", n_years = 1) %>%
-    as_tibble() %>%
-    mutate(pretty_date = lubridate::as_date(pretty_date)) %>%
-    select(pretty_date, close) %>%
-    mutate(spy_rtn_bps = (close/ lag(close) - 1) * 10000,
-           spy_rtn_log = log(close/ lag(close))*10000)
-
-
-HISTORY <- left_join(indra_history, spy_history) %>%
-    filter(pretty_date <= lubridate::ymd(20200731)) %>%
-    mutate(day = lubridate::wday(pretty_date),
-           day_long = lubridate::wday(pretty_date, label = TRUE),
-           day_short = str_sub(day_long, 1, 1),
-           week = lubridate::epiweek(pretty_date),
-           mday = lubridate::mday(pretty_date),
-           month = lubridate::month(pretty_date, label = TRUE))
+# ### we'll also need historical data for SPY
+# 
+# spy_history <- indratools2::td_market_value_traded("SPY", n_years = 1) %>%
+#     as_tibble() %>%
+#     mutate(pretty_date = lubridate::as_date(pretty_date)) %>%
+#     select(pretty_date, close) %>%
+#     mutate(spy_rtn_bps = (close/ lag(close) - 1) * 10000,
+#            spy_rtn_log = log(close/ lag(close))*10000)
+# 
+# 
+# HISTORY <- left_join(indra_history, spy_history) %>%
+#     filter(pretty_date <= lubridate::ymd(20200731)) %>%
+#     mutate(day = lubridate::wday(pretty_date),
+#            day_long = lubridate::wday(pretty_date, label = TRUE),
+#            day_short = str_sub(day_long, 1, 1),
+#            week = lubridate::epiweek(pretty_date),
+#            mday = lubridate::mday(pretty_date),
+#            month = lubridate::month(pretty_date, label = TRUE))
 
 # Define server logic required to draw a histogram
 server <- function(input, output) {
 
     output$distPlot <- render_gt({
-        
-        HISTORY %>%
-            filter(month < lubridate::month(TODAY, label = T)) %>%
-            group_by(month) %>%
-            summarise(indra_rtn_log = sum(indra_rtn_log, na.rm = T),
-                      spy_rtn_log = sum(spy_rtn_log, na.rm = T))
+        indra_history
+        # HISTORY %>%
+        #     filter(month < lubridate::month(TODAY, label = T)) %>%
+        #     group_by(month) %>%
+        #     summarise(indra_rtn_log = sum(indra_rtn_log, na.rm = T),
+        #               spy_rtn_log = sum(spy_rtn_log, na.rm = T))
     })
 }
 
