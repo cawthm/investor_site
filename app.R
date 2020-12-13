a88cc4b3d29a2bc10c089c14895613b50d5ef2fb
Michael Cawthon <michael_cawthon@Michaels-MBP.attlocal.net>
2020-12-13 13:49:22 -0600

    still debugging indra-investor

--- app.R
+++ app.R
@@ -48,29 +48,29 @@ holidays <- lubridate::ymd(paste0("2020", c("0101", "0120", "0217", "0410", "052
 
 
 ### we'll also need historical data for SPY
-
-spy_history <- indratools2::td_market_value_traded("SPY", n_years = 1) %>%
-    as_tibble() %>%
-    mutate(pretty_date = lubridate::as_date(pretty_date)) %>%
-    select(pretty_date, close) %>%
-    mutate(spy_rtn_bps = (close/ lag(close) - 1) * 10000,
-           spy_rtn_log = log(close/ lag(close))*10000)
 # 
-# 
-HISTORY <- left_join(indra_history, spy_history) %>%
-    filter(pretty_date <= lubridate::ymd(20200731)) %>%
-    mutate(day = lubridate::wday(pretty_date),
-           day_long = lubridate::wday(pretty_date, label = TRUE),
-           day_short = str_sub(day_long, 1, 1),
-           week = lubridate::epiweek(pretty_date),
-           mday = lubridate::mday(pretty_date),
-           month = lubridate::month(pretty_date, label = TRUE))
+# spy_history <- indratools2::td_market_value_traded("SPY", n_years = 1) %>%
+#     as_tibble() %>%
+#     mutate(pretty_date = lubridate::as_date(pretty_date)) %>%
+#     select(pretty_date, close) %>%
+#     mutate(spy_rtn_bps = (close/ lag(close) - 1) * 10000,
+#            spy_rtn_log = log(close/ lag(close))*10000)
+# # 
+# # 
+# HISTORY <- left_join(indra_history, spy_history) %>%
+#     filter(pretty_date <= lubridate::ymd(20200731)) %>%
+#     mutate(day = lubridate::wday(pretty_date),
+#            day_long = lubridate::wday(pretty_date, label = TRUE),
+#            day_short = str_sub(day_long, 1, 1),
+#            week = lubridate::epiweek(pretty_date),
+#            mday = lubridate::mday(pretty_date),
+#            month = lubridate::month(pretty_date, label = TRUE))
 
 # Define server logic required to draw a histogram
 server <- function(input, output) {
 
     output$distPlot <- render_gt({
-        HISTORY
+        indra_history
         # HISTORY %>%
         #     filter(month < lubridate::month(Sys.Date(), label = T)) %>%
         #     group_by(month) %>%
